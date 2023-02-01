3a86aa63f4517d64ea00941f5bc97fb836b9441c
Siigismund <lahdenperamiika@gmail.com>
2023-02-01 19:14:42 +0200

    In-class task 2
    Created global header & footer. layout.html & index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    $title = "Homepage";
+    include 'header.php'
+?>
+
+<h1>Homepage of LFC Fan Club</h1>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis accusantium dolorum doloremque iste? Expedita in aspernatur nihil odit? Omnis temporibus quos ipsam, sit qui provident autem quo similique unde voluptatum.</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, tempore laudantium. Ullam provident illum, cumque obcaecati consectetur aperiam perferendis at harum quos temporibus, maiores labore dolor, unde culpa a. Pariatur.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore similique a temporibus quam recusandae voluptas porro doloribus animi nobis voluptate accusamus enim culpa, iure aut quasi quo architecto adipisci maxime.</p>
+
+<?php
+    include 'footer.php'
+?>
